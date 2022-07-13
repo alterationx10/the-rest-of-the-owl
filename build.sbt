a461fb735f17ea89c09a6aaf720085724bd19143
Mark Rudolph <mark@k8ty.app>
2022-07-13 09:48:01 -0400

    make project for websockets

--- build.sbt
+++ build.sbt
@@ -2,30 +2,40 @@ ThisBuild / scalaVersion := "2.13.8"
 ThisBuild / organization := "com.alterationx10"
 
 val commonDependencies = Seq(
-  "io.d11" %% "zhttp" % "2.0.0-RC9", // Brings in ZIO-2.0.0-RC6
-  "io.getquill" %% "quill-zio" % "4.0.0-RC2", // Uses same ZIO version
-  "io.getquill" %% "quill-jdbc-zio" % "4.0.0-RC2",
-  "dev.zio" %% "zio-json" % "0.3.0-RC8",
-  "com.auth0" % "java-jwt" % "4.0.0",
-  "com.h2database" % "h2" % "1.4.199"
+  "io.d11"        %% "zhttp"          % "2.0.0-RC9", // Brings in ZIO-2.0.0-RC6
+  "io.getquill"   %% "quill-zio"      % "4.0.0-RC2", // Uses same ZIO version
+  "io.getquill"   %% "quill-jdbc-zio" % "4.0.0-RC2",
+  "dev.zio"       %% "zio-json"       % "0.3.0-RC8",
+  "com.auth0"      % "java-jwt"       % "4.0.0",
+  "com.h2database" % "h2"             % "1.4.199"
 )
 
-lazy val basics = project.in(file("basics"))
+lazy val basics = project
+  .in(file("basics"))
   .settings(
     name := "zhttp-basics",
     libraryDependencies ++= commonDependencies,
     fork := true
   )
 
-
-lazy val next = project.in(file("intermediate"))
+lazy val next = project
+  .in(file("intermediate"))
   .settings(
     name := "zhttp-next",
     libraryDependencies ++= commonDependencies,
     fork := true
   )
 
-lazy val troto = project.in(file("troto"))
+lazy val extra = project
+  .in(file("extra"))
+  .settings(
+    name := "zhttp-extra",
+    libraryDependencies ++= commonDependencies,
+    fork := true
+  )
+
+lazy val troto = project
+  .in(file("troto"))
   .settings(
     name := "the-REST-of-the-owl",
     libraryDependencies ++= commonDependencies,
---
+++ extra/src/main/scala/com/alterationx10/troto/OwlServer.scala
@@ -0,0 +1,53 @@
+package com.alterationx10.troto
+
+import zhttp.http._
+import zhttp.service.Server
+import zio._
+import java.io.IOException
+import com.alterationx10.troto.middleware.Verbose
+import zhttp.http.middleware.Cors.CorsConfig
+import zhttp.http.middleware.Cors
+
+object OwlServer extends ZIOAppDefault {
+
+  val port: Int = 9001
+
+  val app: Http[Any, Nothing, Request, Response] = Http.collect[Request] {
+    case Method.GET -> !! / "owls"          => Response.text("Hoot!")
+    case Method.GET -> "owls" /: name /: !! =>
+      Response.text(s"$name says: Hoot!")
+  } @@ Middleware.csrfGenerate()
+
+  val zApp: Http[Any, Nothing, Request, Response] =
+    Http.collectZIO[Request] { case Method.POST -> !! / "owls" =>
+      Random.nextIntBetween(3, 6).map(n => Response.text("Hoot! " * n))
+    } @@ Middleware.csrfValidate()
+
+  val authApp: Http[Any, Nothing, Request, Response] = Http.collect[Request] {
+    case Method.GET -> !! / "secret" / "owls" =>
+      Response.text("The password is 'Hoot!'")
+  } @@ Middleware.basicAuth("hooty", "tootie")
+
+  val combined: Http[Any, Nothing, Request, Response] = app ++ zApp ++ authApp
+
+  val config: CorsConfig =
+    CorsConfig(
+      anyOrigin = false,
+      anyMethod = false,
+      allowedOrigins = s => s.equals("localhost:9001"),
+      allowedMethods = Some(Set(Method.GET, Method.POST))
+    )
+
+  val allMiddleware = Verbose.log ++ Middleware.cors(config)
+
+  val wrapped: Http[Any, Throwable, Request, Response] =
+    combined @@ allMiddleware
+
+  val program: ZIO[Console with Clock, Throwable, ExitCode] = for {
+    _ <- Console.printLine(s"Starting server on http://localhost:$port")
+    _ <- Server.start(port, wrapped)
+  } yield ExitCode.success
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    program.provideSomeLayer(Clock.live ++ Console.live)
+}
